bb40829271be4dd6bcbd6d6979270ae2be342900
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-15 16:22:37 -0500

    Create main.py

---
+++ Documentation/Stats/SmashHeroes/main.py
@@ -0,0 +1,203 @@
+[
+	active_class, # Current hero selected
+	assists, # Unused counter
+	assists_normal, # Unused counter
+	classes # Dictionary for further class-specific stats, see classes template
+	coins, # Current coins
+	
+	damage_dealt, # Total damage dealt across all heroes
+	damage_dealt_normal, # Total damage dealt in 1v1v1v1 gamemode
+	damage_dealt_teams, # Total damage dealt in 2v2v2 gamemode
+	damage_dealt_2v2, # Total damage dealt in 2v2 gamemode
+	
+	deaths, # Total deaths
+	deaths_normal, # Total deaths in 1v1v1v1 gamemode
+	deaths_teams, # Total deaths in 2v2v2 gamemode
+	deaths_2v2, # Total deaths in 2v2 gamemode
+	
+	expired_booster, # Boolean whether player has xp booster active
+	expBooster_purchases_10_plays, # Total 10-game xp boosters bought
+	expBooster_purchases_30_plays, # Total 30-game xp boosters bought
+	expBooster_purchases_50_plays, # Total 50-game xp boosters bought
+	expBooster_purchases_100_plays, # Total 100-game xp boosters bought
+	
+	FRIENDS_firstGame, # Millisecond unix timestamp of first game played of the day in party 1v1v1v1 gamemode
+	FRIENDS_gamesDay, # Amount of party 1v1v1v1 games played on the most recent day at least 1 game of party 1v1v1v1 was played
+	friend_losses, # Unused counter
+	friend_wins, # Unused counter
+	friend_wins_normal, # Unused counter
+	
+	games, # Total games played
+	games_monthly_a, # Monthly games played counter A (see oscillation on Hypixel API docs)
+	games_monthly_b, # Monthly games played counter B (see oscillation on Hypixel API docs)
+	games_normal, # Total games played in 1v1v1v1 gamemode
+	games_teams, # Total games played in 2v2v2 gamemode
+	games_weekly_a, # Weekly games played counter A (see oscillation on Hypixel API docs)
+	games_weekly_b, # Weekly games played counter B (see oscillation on Hypixel API docs)
+	games_2v2, # Total games played in 2v2 gamemode
+	
+	kills, # Total kills
+	kills_monthly_a, # Monthly kills counter A (see oscillation on Hypixel API docs)
+	kills_monthly_b, # Monthly kills counter B (see oscillation on Hypixel API docs)
+	kills_normal, # Total kills in 1v1v1v1 gamemode
+	kills_teams, # Total kills in 2v2v2 gamemode
+	kills_weekly_a, # Weekly kills counter A (see oscillation on Hypixel API docs)
+	kills_weekly_b, # Weekly kills counter B (see oscillation on Hypixel API docs)
+	kills_2v2, # Total kills in 2v2 gamemode
+	
+	lastLevel_BOTMUN, # Current level (>=1) of hero "Botmon"
+	lastLevel_CAKE_MONSTER, # Current (>=1) level of hero "Cake Monster"
+	lastLevel_DUSK_CRAWLER, # Current (>=1) level of hero "Void Crawler"
+	lastLevel_FROSTY, # Current level (>=1) of hero "Cryomancer"
+	lastLevel_GOKU, # Current leve (>=1) of hero "Karakot"
+	lastLevel_GREEN_HOOD, # Current level (>=1) of hero "Green Hood"
+	lastLevel_MARAUDER, # Current level (>=1) of hero "Marauder"
+	lastLevel_PUG, # Current level (>=1) of hero "Pug"
+	lastLevel_SANIC, # Current level (>=1) of hero "Shoop"
+	lastLevel_SERGEANT_SHIELD, # Current (>=1) level of hero "Sgt. Shield"
+	lastLevel_SHOOP_DA_WHOOP, # Current (>=1) level of hero "Shoop"
+	lastLevel_SKULLFIRE, # Current level (>=1) of hero "Skullfire"
+	lastLevel_SPODERMAN, # Current level (>=1) of hero "Spooderman"
+	lastLevel_THE_BULK, # Current level (>=1) of hero "Bulk"
+	lastLevel_TINMAN, # Current level (>=1) of hero "Tinman"
+	
+	losses, # Total losses
+	losses_monthly_a, # Monthly losses counter A (see oscillation on Hypixel API docs)
+	losses_monthly_b, # Monthly losses counter B (see oscillation on Hypixel API docs)
+	losses_normal, # Total losses in 1v1v1v1 gamemode
+	losses_teams, # Total losses in 2v2v2 gamemode
+	losses_weekly_a, # Weekly losses counter A (see oscillation on Hypixel API docs)
+	losses_weekly_b, # Weekly losses counter B (see oscillation on Hypixel API docs)
+	losses_2v2, # Total losses in 2v2 gamemode
+	
+	masterArmor_BOTMUN, # Boolean whether player has purchased masterskin for hero "Botmon"
+	masterArmor_CAKE_MONSTER, # Boolean whether player has purchased masterskin for hero "Cake Monster"
+	masterArmor_DUSK_CRAWLER, # Boolean whether player has purchased masterskin for hero "Void Crawler"
+	masterArmor_FROSTY, # Boolean whether player has purchased masterskin for hero "Cryomancer"
+	masterArmor_GENERAL_CLUCK, # Boolean whether player has purchased masterskin for hero "General Cluck"
+	masterArmor_GOKU, # Boolean whether player has purchased masterskin for hero "Karakot"
+	masterArmor_GREEN_HOOD, # Boolean whether player has purchased masterskin for hero "Green Hood"
+	masterArmor_MARAUDER, # Boolean whether player has purchased masterskin for hero "Marauder"
+	masterArmor_PUG, # Boolean whether player has purchased masterskin for hero "Pug"
+	masterArmor_SANIC, # Boolean whether player has purchased masterskin for hero "Sanic"
+	masterArmor_SERGEANT_SHIELD, # Boolean whether player has purchased masterskin for hero "Sgt. Shield"
+	masterArmor_SHOOP_DA_WHOOP, # Boolean whether player has purchased masterskin for hero "Shoop"
+	masterArmor_SKULLFIRE, # Boolean whether player has purchased masterskin for hero "Skullfire"
+	masterArmor_SPODERMAN, # Boolean whether player has purchased masterskin for hero "Spooderman"
+	masterArmor_THE_BULK, # Boolean whether player has purchased masterskin for hero "Bulk"
+	masterArmor_TINMAN, # Boolean whether player has purchased masterskin for hero "Tinman"
+	
+	ONE_V_JUAN_firstGame, # Millisecond unix timestamp of first 1v1 game played of the day on the most recent day one was played
+	ONE_V_JUAN_gamesDay, # Total 1v1 games played of the day on the most recent day one was played
+	
+	one_v_one_losses, # Unused counter
+	one_v_one_losses_normal, # Unused counter
+	one_v_one_wins, # Unused counter
+	one_v_one_wins_normal, # Unused counter
+	
+	packages, # Unused container for cosmetics (Smash Heroes has no cosmetics)
+	
+	pg_BOTMUN, # Current prestige (>=1) of hero "Botmon"
+	pg_CAKE_MONSTER, # Current prestige (>=1) of hero "Cake Monster"
+	pg_DUSK_CRAWLER, # Current prestige (>=1) of hero "Void Crawler"
+	pg_FROSTY, # Current prestige (>=1) of hero "Cryomancer"
+	pg_GENERAL_CLUCK, # Current prestige (>=1) of hero "General Cluck"
+	pg_GOKU, # Current prestige (>=1) of hero "Karakot"
+	pg_GREEN_HOOD, # Current prestige (>=1) of hero "Green Hood"
+	pg_MARAUDER, # Current prestige (>=1) of hero "Marauder"
+	pg_PUG, # Current prestige (>=1) of hero "Pug"
+	pg_SANIC, # Current prestige (>=1) of hero "Sanic"
+	pg_SERGEANT_SHIELD, # Current prestige (>=1) of hero "Sgt. Shield"
+	pg_SHOOP_DA_WHOOP, # Current prestige (>=1) of hero "Shoop"
+	pg_SKULLFIRE, # Current prestige (>=1) of hero "Skullfire"
+	pg_SPODERMAN, # Current prestige (>=1) of hero "Spooderman"
+	pg_THE_BULK, # Current prestige (>=1) of hero "Bulk"
+	pg_TINMAN, # Current prestige (>=1) of hero "Tinman"
+	
+	quits, # Amount of times the player left the game mid-match
+	
+	smashed, # Player deaths via damage
+	smashed_normal, # Player deaths via damage in 1v1v1v1 gamemode
+	smashed_teams, # Player deaths via damage in 2v2v2 gamemode
+	smashed_2v2, # Player deaths via damage in 2v2 gamemode
+	
+	smasher, # Player kills via damage
+	smasher_normal, # Player kills via damage in 1v1v1v1 gamemode
+	smasher_teams, # Player kills via damage in 2v2v2 gamemode
+	smasher_2v2, # Player kills via damage in 2v2 gamemode
+	
+	smashLevel, # Camel-case duplicate of `smash_total_level`
+	smash_total_level, # Total Smash Heroes level (combined level of all heroes purchased, carrying over prestiges)
+	
+	votes_Agora, # Boolean-bugged (0/1) counter for having selected the "Agora" map at least once
+	votes_Apex, # Boolean-bugged (0/1) counter for having selected the "Apex" map at least once
+	votes_Bastion, # Boolean-bugged (0/1) counter for having selected the "Bastion" map at least once
+	votes_Bawk, # Boolean-bugged (0/1) counter for having selected the "Bawk" map at least once
+	votes_Blossom, # Boolean-bugged (0/1) counter for having selected the "Blossom" map at least once
+	votes_Circled, # Boolean-bugged (0/1) counter for having selected the "Circled" map at least once
+	votes_Cobalt, # Boolean-bugged (0/1) counter for having selected the "Cobalt" map at least once
+	votes_Cogsworth, # Boolean-bugged (0/1) counter for having selected the "Cogsworth" map at least once
+	votes_Color Clash, # Boolean-bugged (0/1) counter for having selected the "Color Clash" map at least once
+	votes_Courtyard, # Boolean-bugged (0/1) counter for having selected the "Courtyard" map at least once
+	votes_Cosmos, # Boolean-bugged (0/1) counter for having selected the "Cosmos" map at least once
+	votes_Dojo, # Boolean-bugged (0/1) counter for having selected the "Dojo" map at least once
+	votes_Equinox, # Boolean-bugged (0/1) counter for having selected the "Equinox" map at least once
+	votes_Grave, # Boolean-bugged (0/1) counter for having selected the "Grave" map at least once
+	votes_Grove, # Boolean-bugged (0/1) counter for having selected the "Grove" map at least once
+	votes_Gunmetal, # Boolean-bugged (0/1) counter for having selected the "Gunmental" map at least once
+	votes_Harmony, # Boolean-bugged (0/1) counter for having selected the "Harmony" map at least once
+	votes_Luxor, # Boolean-bugged (0/1) counter for having selected the "Luxor" map at least once
+	votes_Marshland, # Boolean-bugged (0/1) counter for having selected the "Marshland" map at least once
+	votes_Mosaic, # Boolean-bugged (0/1) counter for having selected the "Mosaic" map at least once
+	votes_Reconcile, # Boolean-bugged (0/1) counter for having selected the "Reconcile" map at least once
+	votes_Remains, # Boolean-bugged (0/1) counter for having selected the "Remains" map at least once
+	votes_Shard, # Boolean-bugged (0/1) counter for having selected the "Shard" map at least once
+	votes_Skyline, # Boolean-bugged (0/1) counter for having selected the "Skyline" map at least once
+	votes_Solaris Templum, # Boolean-bugged (0/1) counter for having selected the "Solaris Templum" map at least once
+	votes_Strawberry Towers, # Boolean-bugged (0/1) counter for having selected the "Strawberry Towers" map at least once
+	votes_Swift, # Boolean-bugged (0/1) counter for having selected the "Swift" map at least once
+	votes_Toybox, # Boolean-bugged (0/1) counter for having selected the "Toybox" map at least once
+	votes_Triplets, # Boolean-bugged (0/1) counter for having selected the "Triplets" map at least once
+	
+	wins, # Total wins
+	wins_monthly_a, # Monthly wins counter A (see oscillation on Hypixel API docs)
+	wins_monthly_b, # Monthly wins counter B (see oscillation on Hypixel API docs)
+	wins_normal, # Total wins in 1v1v1v1 gamemode
+	wins_teams, # Total wins in 2v2v2 gamemode
+	wins_weekly_a, # Weekly wins counter A (see oscillation on Hypixel API docs)
+	wins_weekly_b, # Weekly wins counter B (see oscillation on Hypixel API docs)
+	wins_2v2, # Total wins in 2v2 gamemode
+	win_streak, # Broken general winstreak counter
+	
+	xp_BOTMUN, # Current Hero XP for "Botmon" (Resets on level up and prestige) 
+	xp_CAKE_MONSTER, # Current Hero XP for "Cake Monster" (Resets on level up and prestige)
+	xp_DUSK_CRAWLER, # Current Hero XP for "Void Crawler" (Resets on level up and prestige)
+	xp_FROSTY, # Current Hero XP for "Cryomancer" (Resets on level up and prestige)
+	xp_GENERAL_CLUCK, # Current Hero XP for "General Cluck" (Resets on level up and prestige)
+	xp_GOKU, # Current Hero XP for "Karakot" (Resets on level up and prestige)
+	xp_GREEN_HOOD, # Current Hero XP for "Green Hood" (Resets on level up and prestige)
+	xp_MARAUDER, # Current Hero XP for "Marauder" (Resets on level up and prestige)
+	xp_PUG, # Current Hero XP for "Pug" (Resets on level up and prestige)
+	xp_SANIC, # Current Hero XP for "Sanic" (Resets on level up and prestige)
+	xp_SERGEANT_SHIELD, # Current Hero XP for "Sgt. Shield" (Resets on level up and prestige)
+	xp_SHOOP_DA_WHOOP, # Current Hero XP for "Shoop" (Resets on level up and prestige)
+	xp_SKULLFIRE, # Current Hero XP for "Skullfire" (Resets on level up and prestige)
+	xp_SPODERMAN, # Current Hero XP for "Spooderman" (Resets on level up and prestige)
+	xp_THE_BULK, # Current Hero XP for "Bulk" (Resets on level up and prestige)
+	xp_TINMAN, # Current Hero XP for "Tinman" (Resets on level up and prestige)
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
