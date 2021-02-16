93805c3ef6b270d72b600c5f31b146b112bdf996
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-15 20:49:58 -0500

    Create class.py

---
+++ Documentation/Stats/SmashHeroes/class.py
@@ -0,0 +1,95 @@
+[
+	batarang, # Botmon ability "Throw Botarang"
+	bazooka, # General Cluck ability "Shoot Rocket"
+	bite, # Pug ability "Bite"
+	boom, # Sanic ability "Sanic Boom"
+	botmubile, # Botmon ability "Botmobile"
+	boulder, # Bulk ability "BULK THROW!"
+	cake_storm, # Cake Monster ability "Cake Storm"
+	charged_beam, # Shoop ability "Charged Lazor"
+	damage_dealt, # Total damage dealt while using x class
+	damage_dealt_normal, # Total damage dealt while using x class in the 1v1v1v1 gamemode
+	damage_dealt_teams, # Total damage dealt while using x class in the 2v2v2 gamemode
+	damage_dealt_2v2, # Total damage dealt while using x class in the 2v2 gamemode
+	dash, # Sanic ability "Spin Dash"
+	deaths, # Total deaths while using x class
+	deaths_normal, # Total deaths while using x class in the 1v1v1v1 gamemode
+	deaths_teams, # Total deaths while using x class in the 2v2v2 gamemode
+	deaths_2v2, # Total deaths while using x class in the 2v2 gamemode
+	defecake, # Cake Monster ability "Defecake"
+	desert_eagle, # Skullfire ability "Deagle"
+	egg_bazooka, # General Cluck ability "Clusterbuck"
+	fir_ma_lazer, # Shoop ability "FIRIN' MAH LAZOR"
+	flaming_desert_eagle, # Skullfire ability "Flaming Deagle"
+	flying_punch, # Green Hood ability "Flying Punch"
+	force_lightning, # Marauder ability "Lightning"
+	force_pull, # Marauder ability "Grip"
+	freezing_breath, # Cryomancer ability "Freezing Breath"
+	friend_losses, # Total losses while using x class in the party 1v1v1v1 gamemode
+	friend_losses_normal, # Duplicate of `friend_losses`
+	friend_wins, # Total wins while using x class in the party 1v1v1v1 gamemode
+	friend_wins_normal, # Duplicate of `friend_wins`
+	frostbolt, # Cryomancer ability "Frostbolt"
+	kame_beam, # Karakot ability "Kame Do"
+	kills, # Total kills while using x class
+	kills_normal, # Total kills while using x class in the 1v1v1v1 gamemode
+	kills_teams, # Total kills while using x class in the 2v2v2 gamemode
+	kills_2v2, # Total kills while using x class in the 2v2 gamemode
+	ki_blast, # Karakot ability "Ki Blast"
+	games, # Total games played while using x class
+	games_normal, # Total games played while using x class in the 1v1v1v1 gamemode
+	games_teams, # Total games played while using x class in the 2v2v2 gamemode
+	games_2v2, # Total games played while using x class in the 2v2 gamemode
+	grappling_hook, # Botmon ability "Botclaw"
+	grenade, # Skullfire ability "Grenade"
+	homing_missiles, # Tinman ability "Homing Missiles"
+	laser_cannon, # Tinman ability "Shoot Laser"
+	losses, # Total losses while using x class
+	losses_normal, # Total losses while using x class in the 1v1v1v1 gamemode
+	losses_teams, # Total losses while using x class in the 2v2v2 gamemode
+	losses_2v2, # Total losses while using x class in the 2v2 gamemode
+	melee, # Melee attack ability from every kit
+	monster_charge, # Bulk ability "BULK CHARGE!"
+	monster_mash, # Bulk ability "BULK SMASH!"
+	notched_bow, # Green Hood ability "Enhanced Bow"
+	one_v_one_losses, # Total losses while using x class in the 1v1 gamemode
+	one_v_one_losses_normal, # Duplicate of `one_v_one_losses`
+	one_v_one_wins, # Total wins while using x class in the 1v1 gamemode
+	one_v_one_wins_normal, # Duplicate of `one_v_one_wins`
+	onion_cannon, # Sanic ability "Supersanic Ring Cannon"
+	overload, # Tinman ability "Overload"
+	reinforcements, # General Cluck ability "Chicken Army"
+	ricochet, # Sgt. Shield ability "Ricochet"
+	ride_the_lightning, # Shoop ability "Ride the Lightning"
+	rocket_punch, # Tinman ability "Rocket Punch"
+	shield_bash, # Sgt. Shield ability "Shield"
+	shield_quake, # Sgt. Shield ability "Justice Shield"
+	smashed, # Total deaths from damage while using x class
+	smashed_normal, # Total deaths from damage while using x class in the 1v1v1v1 gamemode
+	smashed_teams, # Total deaths from damage while uxing x class in the 2v2v2 gamemode
+	smashed_2v2, # Total deaths from damage while using x class in the 2v2 gamemode
+	smasher, # Total kills from damage while uxing x class
+	smasher_normal, # Total kills from damage while uxing x class in the 1v1v1v1 gamemode
+	smasher_teams, # Total kills from damage while uxing x class in the 2v2v2 gamemode
+	smasher_2v2, # Total kills from damage while uxing x class in the 2v2 gamemode
+	seismic_slam, # Bulk ability "BULK SLAM!"
+	spider_kick, # Spooderman ability "Spooder Kick"
+	spooder_buddies, # Spoorderman ability "Spooder Buddies"
+	static_laser, # Shoop ability "Lightning Bolt"
+	supersonic_bark, # Pug ability "Sonic Bark"
+	swing_pin, # Cake Monster ability "Swing Pin"
+	teleboom, # Void Crawler ability "Teleboom"
+	throw_cake, # Cake Monster ability "Regurgicake"
+	void_slash, # Void Crawler ability "Void Slash"
+	wall_climber, # ???????????????????????????????
+	web_shot, # Spooderman ability "Web Shot"
+	werepug, # Pug ability "Intruders!"
+	wins, # Total wins while using x class
+	wins_normal, # Total wins while using x class in the 1v1v1v1 gamemode
+	wins_teams, # Total wins while using x class in the 2v2v2 gamemode
+	wins_2v2, # Total wins while using x class in the 2v2 gamemode
+	win_streak, # Broken winstreak counter for x class
+	win_streak_normal, # Broken winstreak counter for x class in the 1v1v1v1 gamemode
+	win_streak_teams, # Broken winstreak counter for x class in the 2v2v2 gamemode
+	win_streak_2v2, # Broken winstreak counter for x class in the 2v2 gamemode
+]
