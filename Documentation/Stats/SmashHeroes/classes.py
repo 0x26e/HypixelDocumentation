f7282171d7a7682bcc593758d64a86894600bf03
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-15 20:54:40 -0500

    Create classes.py

---
+++ Documentation/Stats/SmashHeroes/classes.py
@@ -0,0 +1,18 @@
+[
+	BOTMUN, # Botmon hero
+	CAKE_MONSTER, # Cake Monster hero
+	DUSK_CRAWLER, # Void Crawler hero
+	FROSTY, # Cryomancer hero
+	GENERAL_CLUCK, # General Cluck hero
+	GOKU, # Karakot hero
+	GREEN_HOOD, # Green Hood hero
+	MARAUDER, # Marauder hero
+	PUG, # Pug hero
+	THE_BULK, # Bulk hero
+	SANIC, # Sanic hero
+	SERGEANT_SHIELD, # Sgt. Shield hero
+	SHOOP_DA_WHOOP, # Shoop hero
+	SKULLFIRE, # Skullfire hero
+	SPODERMAN, # Spooderman hero
+	TINMAN, # Tinman hero
+]
