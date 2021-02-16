81839fd7a3bddcb08c1a3d0eead8e0191ed2f4ef
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-15 21:32:29 -0500

    Create ability.py

---
+++ Documentation/Stats/SmashHeroes/ability.py
@@ -0,0 +1,21 @@
+[
+	damage_dealt, # Total damage dealt while using x class and y ability
+	damage_dealt_normal, # Total damage dealt while using x class and y ability in the 1v1v1v1 gamemode
+	damage_dealt_teams, # Total damage dealt while using x class and y ability in the 2v2v2 gamemode
+	damage_dealt_2v2, # Total damage dealt while using x class and y ability in the 2v2 gamemode
+	
+	kills, # Total kills while using x class and y ability
+	kills_normal, # Total kills while using x class and y ability in the 1v1v1v1 gamemode
+	kills_teams, # Total kills while using x class and y ability in the 2v2v2 gamemode
+	kills_2v2, # Total kills while using x class and y ability in the 2v2 gamemode
+	
+	smasher, # Total kills with damage while using x class and y ability
+	smasher_normal, # Total kills with damage while using x class and y ability in the 1v1v1v1 gamemode
+	smasher_teams, # Total kills with damage while using x class and y ability in the 2v2v2 gamemode
+	smasher_2v2, # Total kills with damage while using x class and y ability in the 2v2 gamemode
+	
+	smashed, # Total deaths from damage while using x class from y ability
+	smashed_normal, # Total deaths from damage while using x class from y ability in the 1v1v1v1 gamemode
+	smashed_teams, # Total deaths from damage while using x class from y ability in the 2v2v2 gamemode
+	smashed_2v2, # Total deaths from damage while using x class from y ability in the 2v2 gamemode
+]
