5ae40c6d399c4b4bbe2d142d65717b60df5e838e
0x26e <78525013+0x26e@users.noreply.github.com>
2021-02-18 16:51:36 -0500

    Update packages.py

--- Documentation/Stats/Quake/packages.py
+++ Documentation/Stats/Quake/packages.py
@@ -1,3 +1,203 @@
 [
-	
+	achievement_flag_1, # ???
+	achievement_flag_3, # ???
+	anvil_land, # Killsound: Anvil Land
+	barrel.burst, # Weapon Part: Burst barrel
+	barrel.creeper, # Weapon Part: Creeper barrel
+	barrel.large_ball, # Weapon Part: Large ball barrel
+	barrel.small_ball, # Weapon Part: Small ball barrel
+	barrel.star, # Weapon Part: Star barrel
+	bat_death, # Killsound: Bat Death
+	beam.bookworm, # Weapon Part: Bookworm beam
+	beam.bubbles, # Weapon Part: Bubbles beam
+	beam.cloud, # Weapon Part: Cloud beam
+	beam.critical_hit, # Weapon Part: Critical Hit beam
+	beam.firework, # Weapon Part: Firework beam
+	beam.flames, # Weapon Part: Flames beam
+	beam.green_stars, # Weapon Part: Green Stars beam
+	beam.hearts, # Weapon Part: Hearts beam
+	beam.notes, # Weapon Part: Notes beam
+	beam.power, # Weapon Part: Power beam
+	beam.purple_stars, # Weapon Part: Purple Stars beam
+	beam.rainbow, # Weapon Part: Rainbow beam
+	beam.smoke, # Weapon Part: Smoke beam
+	beam.thunderclouds, # Weapon Part: Thunderclouds beam
+	blaze_death, # Killsound: Blaze Death
+	bobhat, # Armor Cosmetic: Bob hat
+	bouncyhat, # Armor Cosmetic: Bouncy hat
+	budderkit, # Armor Cosmetic: Budder chestplate
+	budderkit_boots, # Armor Cosmetic: Budder boots
+	budderkit_leggings, # Armor Cosmetic: Budder leggings
+	cactus, # Armor Cosmetic: Cactus hat
+	case.diamond_hoe, # Weapon Part: 
+	case.gold_hoe, # Weapon Part: 
+	case.iron_hoe, # Weapon Part: 
+	case.shiny_diamond_hoe, # Weapon Part: 
+	case.shiny_gold_hoe, # Weapon Part: 
+	case.shiny_iron_hoe, # Weapon Part: 
+	case.shiny_stone_hoe, # Weapon Part: 
+	case.shiny_wood_hoe, # Weapon Part: 
+	case.stone_hoe, # Weapon Part: 
+	case.wood_hoe, # Weapon Part: 
+	cavemanhat, # Armor Cosmetic: Caveman hat
+	cold_war, # Having bought the map "Cold War" (before October 10th 2013)
+	commander, # Armor Cosmetic: Commander hat
+	commander_boots, # Armor Cosmetic: Commander boots
+	commander_leggings, # Armor Cosmetic: Commander leggings
+	controlfreakhat, # Armor Cosmetic: Control Freak hat
+	cow_hurt, # Killsound: Cow Hurt
+	crafterhat, # Armor Cosmetic: Crafter hat
+	creeper_death, # Killsound: Creeper Death
+	dashpower.dash_power, # Maybe if you have purchased dash power upgrades before they were legacy?
+	dashcooldown.dash_cooldown, # Having purchased level 1 of the dash cooldown perk
+	dashcooldown.dash_cooldown_2, # Having purchased level 2 of the dash cooldown perk
+	dashcooldown.dash_cooldown_3, # Having purchased level 3 of the dash cooldown perk
+	dashcooldown.dash_cooldown_4, # Having purchased level 4 of the dash cooldown perk
+	dashcooldown.dash_cooldown_5, # Having purchased level 5 (maxed) of the dash cooldown perk
+	diamond, # Armor Cosmetic: Diamond hat
+	discohat, # Armor Cosmetic: Disco hat
+	discokit, # Armor Cosmetic: Disco chestplate
+	disco_boots, # Armor Cosmetic: Disco boots
+	disco_leggings, # Armor Cosmetic: Disco leggings
+	dispenser, # Armor Cosmetic: Dispenser hat
+	dragon_growl, # Killsound: Dragon Growl
+	ecologyhat, # Armor Cosmetic: Ecology hat
+	elite, # Armor Cosmetic: Elite hat
+	elite_boots, # Armor Cosmetic: Elite boots
+	elite_leggings, # Armor Cosmetic: Elite leggings
+	enderman_death, # Killsound: Enderman Death
+	engineeringhat, # Armor Cosmetic: Engineering hat
+	exodiahat, # Armor Cosmetic: Quakezodia hat
+	exodiakit, # Armor Cosmetic: Quakezodia chestplate
+	exodia_boots, # Armor Cosmetic: Quakezodia boots
+	exodia_leggings, # Armor Cosmetic: Quakezodia leggings
+	explosivehat, # Armor Cosmetic: Explosive hat
+	fashionistakit, # Armor Cosmetic: Fashionista chestplate
+	fashionistakit_boots, # Armor Cosmetic: Fashionista boots
+	fashionistakit_leggings, # Armor Cosmetic: Fashionista leggings
+	ghast_death, # Killsound: Ghast Death
+	golem_death, # Killsound: Golem Death
+	hipsterhat, # Armor Cosmetic: Hipster hat
+	horse_death, # Killsound: Horse Death
+	hypixelhat, # Armor Cosmetic: Hypixel hat
+	instant_respawn, # Having turned on the setting to instantly respawned
+	invaderkit, # Armor Cosmetic: Invader chestplate
+	invaderkit_boots, # Armor Cosmetic: Invader boots
+	invaderkit_leggings, # Armor Cosmetic: Invader leggings
+	jeeperscreepershat, # Armor Cosmetic: Jeepers Creepers hat
+	lantern, # Armor Cosmetic: Lantern hat
+	level_up, # Killsound: Level Up
+	librarianhat, # Killsound: Librarian hat
+	lighthat, # Armor Cosmetic: Light hat
+	majestic, # Armor Cosmetic: Masjectic chestplate
+	majestichat, # Armor Cosmetic: Majestic hat
+	majestic_boots, # Armor Cosmetic: Majestic boots
+	majestic_leggings, # Armor Cosmetic: Majestic leggings
+	marinekit, # Armor Cosmetic: Marine chestplate
+	marinekit_boots, # Armor Cosmetic: Marine boots
+	marinekit_leggings, # Armor Cosmetic: Marine leggings
+	medievalkit, # Armor Cosmetic: Medieval chestplate
+	medievalkit_boots, # Armor Cosmetic: Medieval boots
+	medievalkit_leggings, # Armor Cosmetic: Medieval leggings
+	melon, # Armor Cosmetic: Melon hat
+	muzzle.clay, # Weapon Part: Clay muzzle
+	muzzle.command_block, # Weapon Part: Command Block muzzle
+	muzzle.diamond, # Weapon Part: Diamond muzzle
+	muzzle.emerald, # Weapon Part: Emerald muzzle
+	muzzle.ender_stone, # Weapon Part: End stone muzzle
+	muzzle.gold, # Weapon Part: Gold muzzle
+	muzzle.iron, # Weapon Part: Iron muzzle
+	muzzle.lapis, # Weapon Part: Lapis muzzle
+	muzzle.melon, # Weapon Part: Melon muzzle
+	muzzle.none, # Weapon Part: Not having a muzzle equipped
+	muzzle.obsidian, # Weapon Part: Obsidian muzzle
+	muzzle.prismarine, # Weapon Part: Prismarine muzzle
+	muzzle.pumpkin, # Weapon Part: Pumpkin muzzle
+	muzzle.quartz, # Weapon Part: Quartz muzzle
+	muzzle.redstone, # Weapon Part: Redstone muzzle
+	muzzle.sea_lantern, # Weapon Part: Sea Lantern muzzle
+	muzzle.soul_sand, # Weapon Part: Soul Sand muzzle
+	muzzle.sponge, # Weapon Part: Sponge muzzle
+	muzzle.wood, # Weapon Part: Wooden muzzle
+	pig_death, # Killsound: Pig Death
+	radianthat, # Armor Cosmetic: Radiant hat
+	redstone, # Armor Cosmetic: Redstone hat
+	revengekit, # Armor Cosmetic: Revenge chestplate
+	revengekit_boots, # Armor Cosmetic: Revenge boots
+	revengekit_leggings, # Armor Cosmetic: Revenge leggings
+	richyrichhat, # Armor Cosmetic: Richy Rich hat
+	showoff, # Armor Cosmetic: Showoff hat
+	sight.black, # Weapon Part: Black laser
+	sight.blue, # Weapon Part: Blue laser
+	sight.diamond, # Weapon Part: Diamond laser
+	sight.emerald, # Weapon Part: Emerald laser
+	sight.gold, # Weapon Part: Gold laser
+	sight.gray, # Weapon Part: Gray laser
+	sight.green, # Weapon Part: Green laser
+	sight.orange, # Weapon Part: Orange laser
+	sight.pink, # Weapon Part: Pink laser
+	sight.purple, # Weapon Part: Purple laser
+	sight.red, # Weapon Part: Red laser
+	sight.silver, # Weapon Part: Silver laser
+	sight.white, # Weapon Part: White laser
+	sight.yellow, # Weapon Part: Yellow laser
+	slimekit, # Armor Cosmetic: Slime chestplate
+	slimekit_boots, # Armor Cosmetic: Slime boots
+	slimekit_leggings, # Armor Cosmetic: Slime leggings
+	snowkit, # Armor Cosmetic: Snow chestplate
+	snowkit_boots, # Armor Cosmetic: Snow boots
+	snowkit_leggings, # Armor Cosmetic: Snow leggings
+	soldier, # Armor Cosmetic: Soldier hat
+	soldier_boots, # Armor Cosmetic: Soldier boots
+	soldier_leggings, # Armor Cosmetic: Soldier leggings
+	spacekit, # Armor Cosmetic: Space chestplate
+	spacekit_boots, # Armor Cosmetic: Space boots
+	spacekit_leggings, # Armor Cosmetic: Space leggings
+	spaceman, # Armor Cosmetic: Spaceman hat
+	specopskit, # Armor Cosmetic: Spec Ops chestplate
+	specopskit_boots, # Armor Cosmetic: Spec Ops boots
+	specopskit_leggings, # Armor Cosmetic: Spec Ops leggings
+	swatkit, # Armor Cosmetic: S.W.A.T chestplate
+	swatkit_boots, # Armor Cosmetic: S.W.A.T boots
+	swatkit_leggings, # Armor Cosmetic: S.W.A.T leggings
+	swegkit, # Armor Cosmetic: Sweg chestplate
+	swegkit_boots, # Armor Cosmetic: Sweg boots
+	swegkit_leggings, # Armor Cosmetic: Sweg leggings
+	tnt, # Armor Cosmetic: Miner hat
+	trigger.five_point_zero, # Weapon Part: 5.0s trigger
+	trigger.nine_point_zero, # Weapon Part: 9.0s trigger
+	trigger.one_point_five, # Weapon Part: 1.5s trigger
+	trigger.one_point_four, # Weapon Part: 1.4s trigger
+	trigger.one_point_one, # Weapon Part: 1.1s trigger
+	trigger.one_point_three, # Weapon Part: 1.3s trigger
+	trigger.one_point_two, # Weapon Part: 1.2s trigger
+	trigger.one_point_zero, # Weapon Part: 1.0s trigger
+	trigger.two_point_five, # Weapon Part: 2.5s trigger
+	trigger.zero_point_eight_five, # Weapon Part: 0.85s trigger
+	trigger.zero_point_nine, # Weapon Part: 0.9s trigger
+	villager_mhm, # Killsound: Villager MHM
+	walkingdeadhat, # Armor Cosmetic: Walking Dead hat
+	win.apple, # Have the achievement `Win while using the Apple Corer`
+	win.basic, # Have the achievement `Win while using the Basic railgun`
+	win.bizarre, # Have the achievement `Win while using the Bizarre`
+	win.bfg, # Have the achievement `Win while using the BFG` 
+	win.bling, # Have the achievement `Win while using the Bling Bling Thing`
+	win.blue_lagoon, # Have the achievement `Win while using the Blue Lagoon`
+	win.budder, # Have the achievement `Win while using the Budder Slapper`
+	win.cookie, # Have the achievement `Win while using the Cookie Cannon`
+	win.creeper, # Have the achievement `Win while using the Creeper railgun`
+	win.dark, # Have the achievement `Win while using the Railgun of Darkness`
+	win.fabled, # Have the achievement `Win while using the Fabled railgun`
+	win.harvester, # Have the achievement `Win while using the Harvester railgun`
+	win.hyper, # Have the achievement `Win while using the Hyper railgun`
+	win.love, # Have the achievement `Win while using the Railgun of Love`
+	win.platinum, # Have the achievement `Win while using the Platinum Smelter`
+	win.reaper, # Have the achievement `Win while using The Reaper`
+	win.redstone, # Have the achievement `Win while using the Redstoner`
+	win.snail, # Have the achievement `Win while using the Snail`
+	win.superior, # Have the achievement `Win while using the Superior railgun`
+	win.turtle, # Have the achievement `Win while using the Turtle`
+	win.worm, # Have the achievement `Win while using the Worm`
+	wither_idle, # Killsound: Wither
+	zombie_woodbreak, # Killsound: Zombie Destroy
 ]
